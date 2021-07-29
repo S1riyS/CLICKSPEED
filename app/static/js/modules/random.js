fd7885487438407a7111a2d4980547e1e84f617b
S1riyS <kirill.ankudinov.94@mail.ru>
2021-07-29 13:21:31 +0500

    Added 2 modules

---
+++ app/static/js/modules/random.js
@@ -0,0 +1,6 @@
+//Рандомное число от min до max
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
---
+++ app/static/js/modules/url_parser.js
@@ -0,0 +1,21 @@
+// Получаем все url параметры
+export function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        params = {},
+        i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!params.hasOwnProperty(n)) params[n] = [];
+        params[n].push(nv.length === 2 ? v : null);
+    }
+    return params;
+}
