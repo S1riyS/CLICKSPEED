7e7cd50961c9dba7b43310156a1e48c292f01689
S1riyS <kirill.ankudinov.94@mail.ru>
2021-07-30 14:08:58 +0500

    var" replaced be "let"

--- app/static/js/modules/url_parser.js
+++ app/static/js/modules/url_parser.js
@@ -1,6 +1,6 @@
 // Получаем все url параметры
 export function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
+    let queryStart = url.indexOf("?") + 1,
         queryEnd = url.indexOf("#") + 1 || url.length + 1,
         query = url.slice(queryStart, queryEnd - 1),
         pairs = query.replace(/\+/g, " ").split("&"),
