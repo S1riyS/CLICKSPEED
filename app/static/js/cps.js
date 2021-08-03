657ae533cec0e990dc2b50dc7e8a668426dee2e0
S1riyS <kirill.ankudinov.94@mail.ru>
2021-08-03 16:55:12 +0500

    Timer in cps.js has been reworked

--- app/static/js/cps.js
+++ app/static/js/cps.js
@@ -25,26 +25,30 @@ function startGame() {
         time = 0;
         isTestStarted = true;
         startTestLoop();
+        clickButton.innerHTML = '';
     }
 }
 clickButton.onclick = startGame;
 clickArea.onclick = startGame;
 
+function updateHTML(time) {
+    let dateTimer = new Date(time);
+    let currentTime =
+        dateTimer.getUTCSeconds() + '.' +
+        ('0' + dateTimer.getUTCMilliseconds()).slice(-3, -1);
+
+    timerElement.innerHTML = currentTime
+    cpsCounter.innerHTML = (clicks / (time / 1000)).toFixed(2);
+}
 // Главная функция
 function startTestLoop() {
     clearInterval(timer);
     timer = setInterval(() => {
         time += 10;
-        let dateTimer = new Date(time);
-        let currentTime = 
-            dateTimer.getUTCSeconds() + '.' + 
-            ('0' + dateTimer.getUTCMilliseconds()).slice(-3, -1);
-        
-        timerElement.innerHTML = currentTime
-        cpsCounter.innerHTML = (clicks / parseFloat(currentTime)).toFixed(2);
-        clickButton.innerHTML = '';
+        updateHTML(time);
         
         if (time >= countdownValue * 1000) {
+            timerElement.innerHTML = countdownValue + ".00"
             clicks = 0;
             cps = 0;
             clickButton.innerHTML = 'Click here to start playing';
