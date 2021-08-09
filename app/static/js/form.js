b6769f7b246803ac43fe349f061f4d992c37b433
S1riyS <kirill.ankudinov.94@mail.ru>
2021-08-09 21:45:50 +0500

    Added show/hide password function, style changes

---
+++ app/static/js/form.js
@@ -0,0 +1,12 @@
+// Показать/скрыть пароль
+$(document).on('click', '.password-control', function() {
+    let inputField = $(this).siblings('.form__input').eq(0);
+
+    if (inputField.attr('type') == 'password') {
+        $(this).addClass('view');
+        inputField.attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        inputField.attr('type', 'password');
+    }
+})
